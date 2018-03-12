531a1078effea778859164f0a1e6dae822d931a4
Bobinson K B <bobinson@agileblaze.com>
2018-03-12 16:07:13 +0530

    remit  a fixed amount periodically on STEEM / GOLOS

---
+++ config.py
@@ -0,0 +1,50 @@
+from steem.steem import Steem
+from send_money import list_load
+
+import logging.config
+
+
+
+PRODUCTION = True
+
+
+
+
+logging.config.fileConfig('logging.conf')
+logging.basicConfig(filename='1ogs.txt',level=logging.DEBUG)
+
+
+'''
+STEEM blockchain
+'''
+
+
+STEEM_NODES = ['https://steemd.privex.io','https://gtg.steem.house:8090','https://rpc.buildteam.io', 'https://steemd.minnowsupportproject.org', 'https://rpc.buildteam.io',
+               'https://api.steemit.com']
+
+active_key = list_load("active_wif.txt")
+account = list_load("accounts.txt")
+ctype = list_load("currency.txt")
+amounts = list_load("amounts.txt")
+
+
+frm_ACCOUNT = "bobinson"
+to_ACCOUNT = str(account[0])
+amount = amounts[0]
+
+CURRENCY = ctype[0]
+print(CURRENCY)
+
+WIF = active_key[0]
+
+
+
+# posting, active - fortunex
+STEEM_INSTANCE = Steem(keys=[WIF, WIF], nodes=STEEM_NODES, wif=active_key[0])
+
+
+# END STEEM BLOCKCHAIN
+
+'''
+EOS blockchain
+'''
---
+++ send_money.py
@@ -0,0 +1,56 @@
+from steem.steem import Steem
+
+
+import logging # This module is thread safe.
+from steem.post import Post
+from datetime import datetime
+import time
+import os, sys, csv
+import inspect
+from datetime import date, timedelta
+import steem
+import ast
+import random
+import config
+
+
+def list_load(listfile: object) -> object:
+    with open(listfile, 'r') as readstuff:
+        listvar = []
+        reader = csv.reader(readstuff)
+
+        for rows in reader:
+            v = rows[0]
+            listvar.append(v)
+
+    return listvar
+
+
+# TODO:
+def send_cash(amount):
+    if config.CURRENCY == "STEEM":
+        stat = config.STEEM_INSTANCE.transfer(config.to_ACCOUNT, amount, "STEEM", memo="mb", account=config.frm_ACCOUNT)
+    if config.CURRENCY == "SBD":
+        stat = config.STEEM_INSTANCE.transfer(config.to_ACCOUNT, amount, "SBD", memo="mb", account=config.frm_ACCOUNT)
+
+    return stat
+
+
+
+if __name__ == '__main__':
+    stat = "ERR0R1"
+    while True:
+        try:
+            stat = send_cash(config.amount)
+        except Exception as e:
+            logging.error(repr(e))
+        except TypeError as te:
+            stat = te.args[0]
+            if te.args[0] == "'NoneType' object is not iterable":
+                pass
+        if stat != "ERR0R1":
+            logging.info(stat)
+            if stat['operations']:
+                break
+        if stat == "ERR0R1":
+            logging.error(stat)
